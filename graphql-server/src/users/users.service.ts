bc61771b16a2999708c4ce78f10b88bce7b81ef0
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2024-03-16 07:56:19 +0000

    first commit

---
+++ graphql-server/src/users/users.service.ts
@@ -0,0 +1,38 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { User } from './users.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
+
+  async create(input: CreateUserDTO): Promise<User> {
+    const user = this.userRepository.create(input);
+
+    return await this.userRepository.save(user);
+  }
+
+  async findOne(id: number): Promise<User> {
+    return this.userRepository.findOneOrFail({ where: { id } });
+  }
+
+  async remove(id: number): Promise<Boolean> {
+    const user = await this.userRepository.findOne({ where: { id } });
+
+    if (!user) {
+      throw new BadRequestException();
+    }
+
+    await this.userRepository.delete(user);
+
+    return true;
+  }
+}
